9016b1c929179b1005a08daf954f9806816eec9d
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-04-24 14:11:45 +0300

    Fix container daemonization

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /go/src/github.com/goforbroke1006/dataoverseersvc/
-dep ensure -v
-go run cmd/dataoverseersvc/main.go -log-file=/code/daemon.log
+#dep ensure -v
+go run cmd/dataoverseersvc/main.go -log-file=./daemon.log -сfg-file=./config-docker.yml
